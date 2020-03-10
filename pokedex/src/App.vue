fad8c4f4001aad9926e6efda2eddf5cf8be9b239
Jacob Roberts <jake.j.rob@gmail.com>
2020-03-09 23:24:09 -0600

    Added Bootstrap Vue CLI Plugin

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,31 +1,147 @@
 <template>
-  <div id="app">
-    <div class="menu" id="menu">
-      <div class="brand" id="brand">
-        <router-link to="/">
-          <img src="/images/logo.png" />
-        </router-link>
-      </div>
-      <div class="side" id="side">
-        <router-link to="/browse">
-          <div class="browse">
-            <img src="/images/globe.png" />
-            <p>Browse</p>
+  <div id="app" class="d-flex flex-column h-100">
+    <!-- Top Navbar -->
+    <header>
+      <b-navbar
+        type="dark"
+        variant="dark"
+        toggleable="lg"
+        class="fixed-top flex-lg-nowrap p-0 shadow"
+      >
+        <b-navbar-brand class="col-lg-2 col-md-12 mr-0" to="/">
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 400 48"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            preserveAspectRatio="xMidYMid"
+          >
+            <g fill="#ffffff">
+              <g transform="translate(125.848739, 10.756303)">
+                <text
+                  class=""
+                  x="25%"
+                  y="50%"
+                  dominant-baseline="middle"
+                  text-anchor="middle"
+                  font-size="40px"
+                >
+                  Pokemon
+                </text>
+              </g>
+            </g>
+          </svg>
+        </b-navbar-brand>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapseExample"
+          aria-controls="collapseExample"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="ml-auto mr-md-4 navbar-nav px-3 flex-row">
+          <li class="nav-item dropdown profile">
+            <a class="nav-link disabled" href="#">
+              <img
+                src="https://avatars1.githubusercontent.com/u/1888257?s=460&v=4"
+              />
+            </a>
+          </li>
+        </ul>
+      </b-navbar>
+    </header>
+    <!-- Container to hold content and sidebar -->
+    <div class="container-fluid">
+      <div class="row">
+        <!-- Sidebar -->
+        <nav
+          class="col-lg-2 collapse d-lg-block bg-light sidebar"
+          id="collapseExample"
+        >
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="nav-icon feather-home"
+                  >
+                    <path
+                      d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"
+                    ></path>
+                    <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                  </svg>
+                  My Pokemon <span class="sr-only">(current)</span>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/browse">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="nav-icon feather-file"
+                  >
+                    <path
+                      d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"
+                    ></path>
+                    <polyline points="13 2 13 9 20 9"></polyline>
+                  </svg>
+                  All Pokemon
+                </router-link>
+              </li>
+            </ul>
           </div>
-        </router-link>
-        <router-link to="/cart">
-          <div class="cart">
-            <img src="/images/love.png" />
-            <p>{{ itemsInCart }} items</p>
-          </div>
-        </router-link>
+        </nav>
+        <main role="main" class="ml-md-auto col-lg-10 px-4">
+          <noscript>
+            <strong
+              >We're sorry but <%= htmlWebpackPlugin.options.title %> doesn't
+              work properly without JavaScript enabled. Please enable it to
+              continue.</strong
+            >
+          </noscript>
+          <router-view />
+        </main>
       </div>
     </div>
-    <router-view />
+    <footer class="footer mt-auto py-3 bg-dark shadow">
+      <div class="container">
+        <a
+          class="offset-lg-2 col-lg-10 text-light"
+          href="https://github.com/Jacob-Roberts/school-repository/tree/master/pokedex"
+          >Link to Github Repo</a
+        >
+      </div>
+    </footer>
   </div>
 </template>
 
 <style>
+.navbar-brand {
+  height: 48px;
+}
+
 #app {
   margin-top: 10px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
